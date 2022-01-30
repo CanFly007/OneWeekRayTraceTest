0e100d3e5ef0f662a41497fee49a8e9d95e64cff
MrHe <314373866@qq.com>
2022-01-30 20:58:17 +0800

    增加射线，从左上开始按Y轴画

--- MainProject.cpp
+++ MainProject.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
 
-#include"vec3.h"
+#include"ray.h"
+
+vec3 ray_color(const ray& r)
+{
+	vec3 unit_direction = unit_vecotr(r.direction());
+	auto t = (unit_direction.y() + 1.0) * 0.5;
+	return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
 
 void main()
 {
@@ -9,12 +16,20 @@ void main()
 
 	std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 	
+	vec3 lower_left_corner(-2.0, -1.0, -1.0);
+	vec3 horizontal(4.0, 0.0, 0.0);
+	vec3 vertical(0.0, 2.0, 0.0);
+	vec3 origin(0.0, 0.0, 0.0);
 	for (int j = image_height - 1; j >= 0; j--)
 	{
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
 		for (int i = 0; i < image_width; i++)
 		{
-			vec3 color(double(i) / image_width, double(j) / image_height, 1);
+			auto u = double(i) / image_width;
+			auto v = double(j) / image_height;
+			ray r(origin, lower_left_corner + horizontal * u + vertical * v);
+			
+			vec3 color = ray_color(r);
 			color.write_color(std::cout);
 		}
 	}
