cfc65d10d91b058e08bb300592d87bb08a43404f
He <314373866@qq.com>
2022-02-09 20:24:45 +0800

    测试git branch xxx,git branch -a,git checkout xxx,git push origin xxx

--- MainProject.cpp
+++ MainProject.cpp
@@ -80,7 +80,7 @@ hittable_list random_scene()
 void main()
 {
 	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 1200;
+	const int image_width = 120;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 	const int samplers_per_pixel = 100;
 	const int max_depth = 50;
