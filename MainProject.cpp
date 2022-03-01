276128365512ad1f0be8995958c815b8cbe79d75
He <314373866@qq.com>
2022-03-01 14:03:25 +0800

    在cornelBox内增加两个立方体

--- MainProject.cpp
+++ MainProject.cpp
@@ -2,7 +2,7 @@
 
 #include "rtweekend.h"
 
-#include "aarect.h"
+#include "box.h"
 #include "bvh.h"
 #include "camera.h"
 #include "hittable_list.h"
@@ -135,19 +135,25 @@ hittable_list cornell_box()
 	shared_ptr<lambertian> green = make_shared<lambertian>(make_shared<constant_texture>(vec3(0.12, 0.45, 0.15)));
 	shared_ptr<diffuse_light> light = make_shared<diffuse_light>(make_shared<constant_texture>(vec3(15, 15, 15)));
 
+	//Ç½
 	objects.add(make_shared<yz_rect>(0, 555, 0, 555, 555, green));
 	objects.add(make_shared<yz_rect>(0, 555, 0, 555, 0, red));
-	objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
 	objects.add(make_shared<xz_rect>(0, 555, 0, 555, 0, white));
 	objects.add(make_shared<xy_rect>(0, 555, 0, 555, 555, white));
 	objects.add(make_shared<xz_rect>(0, 555, 0, 555, 555, white));
+	//µÆ
+	objects.add(make_shared<xz_rect>(213, 343, 227, 332, 554, light));
+	//Á½¸öºÐ×Ó
+	objects.add(make_shared<box>(vec3(130, 0, 65), vec3(295, 165, 230), white));
+	objects.add(make_shared<box>(vec3(265, 0, 295), vec3(430, 330, 460), white));
+
 	return objects;
 }
 
 void main()
 {
 	const auto aspect_ratio = 16.0 / 9.0;
-	const int image_width = 300;
+	const int image_width = 1200;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 	const int samplers_per_pixel = 100;
 	const int max_depth = 50;
